edc6cf710259e4ecb3aa521309c2b1af0571a4e2
ujj-wal-g <ujjwal.g@codestoresolutions.com>
2022-02-01 18:55:48 +0530

    Added swagger implementation

---
+++ SwaggerDemo/SwaggerDemo/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swagger.Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/Customer
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public string   Get(int id)
+        {
+            return "value" ;
+        }
+
+        // POST: api/Customer
+        [HttpPost]
+        public string Post([FromBody] string value)
+        {
+            return value;
+        }
+
+        // PUT: api/Customer/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
